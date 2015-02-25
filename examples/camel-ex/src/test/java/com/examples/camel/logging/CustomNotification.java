1e5e87e9586fec9404387b61379a4ca295e79b74
Manoj <manojiitkumar93@gmail.com>
2015-02-25 17:12:42 +0530

    Added other logging feature.

---
+++ examples/camel-ex/src/test/java/com/examples/camel/logging/CustomNotification.java
@@ -0,0 +1,55 @@
+package com.examples.camel.logging;
+
+import java.util.Date;
+import java.util.EventObject;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.management.event.ExchangeCompletedEvent;
+import org.apache.camel.management.event.ExchangeSendingEvent;
+import org.apache.camel.management.event.ExchangeSentEvent;
+import org.apache.camel.support.EventNotifierSupport;
+
+public class CustomNotification extends EventNotifierSupport {
+
+	@Override
+	public void notify(EventObject event) throws Exception {
+		// TODO Auto-generated method stub
+		if (event instanceof ExchangeSentEvent) {
+			ExchangeSentEvent sent = (ExchangeSentEvent) event;
+			log.info(">>>>   Took " + sent.getTimeTaken());
+
+		}
+		if (event instanceof ExchangeCompletedEvent) {
+			ExchangeCompletedEvent exchangeCompletedEvent = (ExchangeCompletedEvent) event;
+			Exchange exchange = exchangeCompletedEvent.getExchange();
+			String routeId = exchange.getFromRouteId();
+			Date created = ((ExchangeCompletedEvent) event).getExchange()
+					.getProperty(Exchange.CREATED_TIMESTAMP, Date.class);
+			// calculate elapsed time
+			Date now = new Date();
+			long elapsed = now.getTime() - created.getTime();
+			log.info(">>>   Took " + elapsed
+					+ " millis for the exchange on the route : " + routeId);
+
+		}
+		if (event instanceof ExchangeSendingEvent) {
+			ExchangeSendingEvent sending = (ExchangeSendingEvent) event;
+			log.info("SendingEndPoint  " + sending.getEndpoint());
+			Exchange exchange = sending.getExchange();
+			String routeId = exchange.getFromRouteId();
+			Date created = ((ExchangeSendingEvent) event).getExchange()
+					.getProperty(Exchange.CREATED_TIMESTAMP, Date.class);
+			Date now = new Date();
+			long elapsed = now.getTime() - created.getTime();
+			log.info(">>> Took " + elapsed);
+
+		}
+
+	}
+
+	@Override
+	public boolean isEnabled(EventObject event) {
+		return true;
+	}
+
+}
---
+++ examples/camel-ex/src/test/java/com/examples/camel/logging/NotificationLogging.java
@@ -0,0 +1,42 @@
+package com.examples.camel.logging;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class NotificationLogging {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		CamelContext context = new DefaultCamelContext();
+		context.getManagementStrategy().addEventNotifier(
+				new CustomNotification());
+		context.setStreamCaching(true);
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				// TODO Auto-generated method stub
+				from("jetty:http://127.0.0.1:2000/order1").routeId("Order1")
+						.to("http://127.0.0.1:5000/order?bridgeEndpoint=true&throwExceptionOnFailure=false")
+						.process(new Processor() {
+
+							@Override
+							public void process(Exchange exchange)
+									throws Exception {
+								// TODO Auto-generated method stub
+								String body = exchange.getIn().getBody(String.class);
+								body  = body +"extra text added along with body";
+								exchange.getOut().setBody(body);
+
+							}
+						});
+			}
+		});
+		context.start();
+		Thread.sleep(1500);
+	}
+
+}
